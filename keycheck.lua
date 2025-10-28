2e5aa074a5281760911bc73af770ef15548da67e
VortexScripting <77008143+IRSILVER@users.noreply.github.com>
2025-10-28 15:53:59 +0100

    Update keycheck.lua

--- keycheck.lua
+++ keycheck.lua
@@ -3,11 +3,4 @@ local validKeys = {
     [987654321] = "checking2",
 }
 
-local userId = tonumber(game:GetService("Players").LocalPlayer.UserId)
-local key = _GET["key"]
-
-if validKeys[userId] and validKeys[userId] == key then
-    return true
-else
-    return false
-end
+return validKeys
